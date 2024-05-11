8ff6c918c4d9c304b4630981302ba0afede6c313
shuhaib-ahmed28 <ahmedshuhaib28@gmail.com>
2024-05-11 09:46:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const btnCart=document.querySelector('#cart-icon');
+const cart=document.querySelector('.cart');
+const btnClose=document.querySelector('#cart-close');
+
+btnCart.addEventListener('click',()=>{
+    cart.classList.add('cart-active')
+})
+btnClose.addEventListener('click',()=>{
+    cart.classList.remove('cart-active')
+})
+
+//to remove items from the cart
+
+document.addEventListener('DOMContentLoaded', loadfood())
+function loadfood(){
+    removeItemFromCart();
+    console.log('hey')
+}
+
+function removeItemFromCart(){
+    let btnRemove=document.querySelectorAll('.cart-remove');
+    btnRemove.forEach((btn)=>{
+        btn.addEventListener('click', removeItem() )
+    });
+
+
+}   
+function removeItem(){
+    this.parentElement.remove();
+}
+
+
+// function AddToCart (){
+//     let btnAddToCart= document.querySelectorAll('.btn');
+//     btnAddToCart.forEach((btnn)=>{
+//         btnn.addEventListener('click' , cartItems())
+//     });
+//     function cartItems(){
+//         this.parentElement.add();
+//     }
+// }
